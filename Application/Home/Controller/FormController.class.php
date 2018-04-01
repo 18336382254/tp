2085d8ced8d522214bf599291c39a3fd8b32de87
王灿 <745811947@qq.com>
2018-04-01 16:02:19 +0800

    查询语言

--- Application/Home/Controller/FormController.class.php
+++ Application/Home/Controller/FormController.class.php
@@ -70,5 +70,66 @@ class FormController extends Controller{
         else{
             $this->error($Form->getError());
         }
-    }
+     }
+        //查询语言
+        public function testSql(){
+            //传统的,字符串作为查询条件
+            //$Data = M('Data');
+            //$res = $Data->where('id = 1')->select();
+            //var_dump($res);
+            //数组的方式查询数据库
+//            $Data = M('Data');
+//            $con['id'] = 1;
+//            $con['data'] = 'php';
+//            //定义查询逻辑默认是AND,这里写OR,表示或
+//            $con['_logic'] = 'OR';
+//            $res = $Data->where($con)->select();
+//            var_dump($res);
+            //使用对象的方式查询
+//            $Data = M('Data');
+//            $condition = new \stdClass();
+//            $condition->id = 1;
+//            $condition->data = 'thinkphp';
+//            $res = $Data->where($condition)->select();
+//            var_dump($res);
+            //表达式查询
+            /*eq 等于
+             *neq不等于
+             *gt大于
+             *egt大于等于
+             *lt小于
+             *elt小于等于
+             *like模糊查询
+             *between区间查询
+             *in in查询
+             *exp   表达式查询
+             */
+//            $Data = M('Data');
+//            $condition['data'] = array('like','think');
+//            $res = $Data->where($condition)->select();
+//            var_dump($res);
+            //统计查询
+            /*
+             * count统计数量,根据字段名
+             * max根据字段名,获取最大值
+             * min根据字段名,获取最小值
+             * avg根据字段名,获取平均值
+             * sum根据字段名获取总分
+             */
+//            $Data = M('Data');
+//            $res = $Data->count('id');
+//            var_dump($res);
+//            echo '<br/>';
+//            $res = $Data->max('id');
+//            var_dump($res);
+//            echo '<br/>';
+//            $res = $Data->min('id');
+//            var_dump($res);
+//            echo '<br/>';
+//            $res = $Data->avg('id');
+//            var_dump($res);
+//            echo '<br/>';
+//            $res = $Data->sum('id');
+//            var_dump($res);
+        }
 }
